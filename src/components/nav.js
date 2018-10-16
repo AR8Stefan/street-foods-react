3eca04e89400dfa988e522c8a491fb323fc524c1
AR8Stefan <stefan.piatkov@gmail.com>
2018-10-16 18:21:58 -0500

    setup

---
+++ src/components/nav.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Nav extends Component {
+  render() {
+    return (
+      <nav className="nav">
+        test
+      </nav>
+    );
+  }
+}
+
+export default Nav;
